e3fea8a8b34ce7c5c57bdabad504d3d39a56b884
ionnotion <adryan.rahadianto@gmail.com>
2023-02-17 14:17:16 +0700

    init modules and fiber instance

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main()  {
-	
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/ionnotion/fiber-product-api/models"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(models.Response{Message: "Hello World"})
+	})
+
+	app.Listen(":8080")
 }
